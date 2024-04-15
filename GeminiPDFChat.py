e08d25faa3c922b8c6ebed2c3c2a7b7b275c065c
rajivraghu <rajiv.vedavyas@gmail.com>
2024-04-15 19:39:57 +0530

    Create GeminiPDFChat.py

---
+++ GeminiPDFChat.py
@@ -0,0 +1,89 @@
+import os
+from PyPDF2 import PdfReader
+import streamlit as st
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+# read all pdf files and return text
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+# split text into chunks
+
+
+def clear_chat_history():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "upload some pdfs and ask me a question"}]
+
+
+def user_input(user_question):
+        
+    genai.configure(api_key="xxxyy")
+    model = genai.GenerativeModel(model_name="models/gemini-1.5-pro-latest")
+    extracted_text = st.session_state.get('pdftext', '')
+    full_input = f" Answer the question:{user_question} for the input..{extracted_text} . Note:!!! Do not add additional information than what is asked for.  Do not makeup the answers.If you dont know the answer say Unable to find the required info in the document."
+    output = model.generate_content(full_input)
+    return output
+
+
+def main():
+    st.set_page_config(
+        page_title="Gemini PDF Chatbot",
+        page_icon="🤖"
+    )
+
+    # Sidebar for uploading PDF files
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader(
+            "Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                st.session_state['pdftext'] = raw_text
+                st.success("Done")
+
+    # Main content area for displaying chat messages
+    st.title("Chat with PDF files using Gemini🤖")
+    st.write("Welcome to the chat!")
+    st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+    # Chat input
+    # Placeholder for chat messages
+
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [
+            {"role": "assistant", "content": "upload some pdfs and ask me a question"}]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+
+    # Display chat messages and bot response
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = user_input(prompt)
+                placeholder = st.empty()
+                full_response =response.text
+                placeholder.markdown(full_response)
+                placeholder.markdown(full_response)
+        if response is not None:
+            message = {"role": "assistant", "content": full_response}
+            st.session_state.messages.append(message)
+
+
+if __name__ == "__main__":
+    main()
